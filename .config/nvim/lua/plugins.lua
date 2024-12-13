4ac102219edaa0e767b289d0630100ccd9febc79
Dylan Palmieri <dgpalmieri@gmail.com>
2024-12-13 10:41:55 -0900

    feat: add gitsigns mappings

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -43,7 +43,20 @@ return {
     {
         'lewis6991/gitsigns.nvim',
         config = function()
-            require("gitsigns").setup()
+            require("gitsigns").setup{
+                on_attach = function(bufnr)
+                    local gitsigns = require('gitsigns')
+
+                    local function map(mode, l, r, opts)
+                        opts = opts or {}
+                        opts.buffer = bufnr
+                        vim.keymap.set(mode, l, r, opts)
+                    end
+
+                    map('n', '<leader>hb', function() gitsigns.blame_line{full=true} end)
+                    map('n', '<leader>tb', gitsigns.toggle_current_line_blame)
+                end
+            }
         end,
     },
 
